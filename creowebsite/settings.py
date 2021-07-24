ed2b0c63dfdad976ad51282de1b2b6140b48c6d4
basjakov <arman.antonyan12@gmail.com>
2021-07-24 19:54:22 +0400

    second[1]

--- creowebsite/settings.py
+++ creowebsite/settings.py
@@ -126,10 +126,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'creowebsite/static')
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL = '/assets/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
